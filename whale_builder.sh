c2d04ddb4f8e86ed31536fe7fd910f66075215b5
dreamPathsProjekt <dream.paths.projekt@gmail.com>
2018-04-18 20:54:37 +0300

    Fix registry tag

--- whale_builder.sh
+++ whale_builder.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 docker build -t whalefisher . && \
-docker tag whalefisher  registry.greece:5001/whalefisher:"$1" && \
-docker tag whalefisher  registry.greece:5001/whalefisher:latest && \
-docker push registry.greece:5001/whalefisher:"$1" && \
-docker push registry.greece:5001/whalefisher:latest
-docker service update whalefisher_exporter --force --image registry.greece:5001/whalefisher:"$1"
+docker tag whalefisher  registry.dream:5001/whalefisher:"$1" && \
+docker tag whalefisher  registry.dream:5001/whalefisher:latest && \
+docker push registry.dream:5001/whalefisher:"$1" && \
+docker push registry.dream:5001/whalefisher:latest
+docker service update whalefisher_exporter --force --image registry.dream:5001/whalefisher:"$1"
 # docker stack deploy --compose-file whale-fisher.yml whalefisher
 exit 0
