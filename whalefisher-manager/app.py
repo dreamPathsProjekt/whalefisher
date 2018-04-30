bba0935369ea71b34f79723308e49abf2625ed4f
Ioannis Dritsas <i.dritsas@jrtechnologies.com>
2018-04-30 11:32:12 +0300

    Add threaded server config

--- whalefisher-manager/app.py
+++ whalefisher-manager/app.py
@@ -126,4 +126,4 @@ def get_logs_compact():
 
 if __name__ == "__main__":
     # app.run(debug=True, host='0.0.0.0', port=5000, use_evalex=False, threaded=False)
-    socketio.run(app,  host='0.0.0.0', port=5000, debug=False)
+    socketio.run(app,  host='0.0.0.0', port=5000, debug=False, threaded=True)
