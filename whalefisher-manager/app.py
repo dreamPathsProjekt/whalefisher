6c0402773db43427643420851772e4f0ce9a528e
Ioannis Dritsas <i.dritsas@jrtechnologies.com>
2018-04-30 14:27:35 +0300

    Add debug=True to manager only

--- whalefisher-manager/app.py
+++ whalefisher-manager/app.py
@@ -129,4 +129,4 @@ def test_streaming_logs():
 
 if __name__ == "__main__":
     # app.run(debug=True, host='0.0.0.0', port=5000, use_evalex=False, threaded=False)
-    socketio.run(app,  host='0.0.0.0', port=5000, debug=False)
+    socketio.run(app,  host='0.0.0.0', port=5000, debug=True)
