dce7a1fc4c4b43f81670068a456c21e96bc40714
Ioannis Dritsas <i.dritsas@jrtechnologies.com>
2018-05-02 16:53:34 +0300

    Set chunk_size=1 on manager routes

--- whalefisher-manager/app.py
+++ whalefisher-manager/app.py
@@ -138,7 +138,7 @@ def get_logs_by_task_id_stream(name, id):
         stream=True)
 
     def generate_from_provider():
-        for line in request_stream.iter_lines(chunk_size=1024, decode_unicode=True):
+        for line in request_stream.iter_lines(chunk_size=1, decode_unicode=True):
             # yield str(line).strip() + '\n'
             yield line + '\n'
 
@@ -160,7 +160,7 @@ def get_logs_by_task_id_stream_tail(name, id, lines):
         stream=True)
 
     def generate_from_provider():
-        for line in request_stream.iter_lines(decode_unicode=True):
+        for line in request_stream.iter_lines(chunk_size=1, decode_unicode=True):
             # yield str(line).strip() + '\n'
             yield line + '\n'
 
