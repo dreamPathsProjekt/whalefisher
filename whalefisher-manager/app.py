e5e0f5654eae143552461da89806102edb7c7dcb
Ioannis Dritsas <i.dritsas@jrtechnologies.com>
2018-05-03 15:56:15 +0300

    Cleanup manager, provider

--- whalefisher-manager/app.py
+++ whalefisher-manager/app.py
@@ -263,24 +263,6 @@ def get_service_logs_tail(name, lines):
             tail=lines
         ), mimetype='text/plain')
 
-# @app.route('/service/<string:name>/tasks/<string:id>/logs/stream/compact')
-# def stream_logs_from_compact(name, id):
-#     cont_json = get_container_by_task_id(name, id)
-
-#     if cont_json is None:
-#         abort(404)
-
-#     def stream_compact():
-#         while True:
-#             request_json = requests.get('http://{}:{}/container/{}/logs/compact'.format(
-#                 cont_json['node_ip'],
-#                 cont_json['node_port'],
-#                 cont_json['container_id']))
-#             yield json.dumps(request_json.json())
-#             time.sleep(1)
-
-#     return Response(stream_compact(), mimetype='application/json')
-
 
 @app.route('/node')
 def get_nodes_hostnames():
@@ -290,27 +272,6 @@ def get_nodes_hostnames():
 @app.route('/node/current')
 def get_current_node():
     return jsonify(get_current_nodename())
-
-
-@app.route('/teststream')
-def test_streaming_logs():
-    # Below to be handled by middleware module
-    req_get_name = requests.get('http://10.132.0.28:8086/container')
-
-    cont_id = ''
-    for container in req_get_name.json():
-        if str(container['name']).startswith('ifg_logstash'):
-            cont_id = container['id']
-
-    request_stream = requests.get('http://10.132.0.28:8086/container/{}/logs/tail/20'.format(cont_id), stream=True)
-
-    # for line in request_stream.iter_content(chunk_size=2048, decode_unicode=True):
-    #     print(line)
-    def generate_from_provider():
-        for line in request_stream.iter_lines(chunk_size=2048, decode_unicode=True):
-            yield str(line).strip() + '\n'
-
-    return Response(generate_from_provider(), mimetype='text/plain')
 
 
 if __name__ == "__main__":
