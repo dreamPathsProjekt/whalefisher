91a677e3cfec0b4ffc969934c77eca3a9e0e7e49
dreamPathsProjekt <dream.paths.projekt@gmail.com>
2018-05-19 01:07:27 +0300

    Update build script and .gitignore

--- whale_builder.sh
+++ whale_builder.sh
@@ -1,16 +1,19 @@
 #!/bin/bash
 
+# Edit below line to your own private registry
+DOCKER_REGISTRY=registry.dream:5001
+
 docker build -t whalefisher-manager ./whalefisher-manager/ && \
-docker tag whalefisher-manager  registry.dream:5001/whalefisher-manager:"$1" && \
-docker tag whalefisher-manager  registry.dream:5001/whalefisher-manager:latest && \
-docker push registry.dream:5001/whalefisher-manager:"$1" && \
-docker push registry.dream:5001/whalefisher-manager:latest
+docker tag whalefisher-manager  $DOCKER_REGISTRY/whalefisher-manager:"$1" && \
+docker tag whalefisher-manager  $DOCKER_REGISTRY/whalefisher-manager:latest && \
+docker push $DOCKER_REGISTRY/whalefisher-manager:"$1" && \
+docker push $DOCKER_REGISTRY/whalefisher-manager:latest
 
 docker build -t whalefisher-data_provider ./whalefisher-data_provider/ && \
-docker tag whalefisher-data_provider  registry.dream:5001/whalefisher-data_provider:"$1" && \
-docker tag whalefisher-data_provider  registry.dream:5001/whalefisher-data_provider:latest && \
-docker push registry.dream:5001/whalefisher-data_provider:"$1" && \
-docker push registry.dream:5001/whalefisher-data_provider:latest
+docker tag whalefisher-data_provider  $DOCKER_REGISTRY/whalefisher-data_provider:"$1" && \
+docker tag whalefisher-data_provider  $DOCKER_REGISTRY/whalefisher-data_provider:latest && \
+docker push $DOCKER_REGISTRY/whalefisher-data_provider:"$1" && \
+docker push $DOCKER_REGISTRY/whalefisher-data_provider:latest
 
 # docker stack deploy --compose-file whale-fisher.yml whalefisher
 exit 0
