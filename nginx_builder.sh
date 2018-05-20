dbb35f1725092da31e2cd27de271acc59dcc1037
dreamPathsProjekt <dream.paths.projekt@gmail.com>
2018-05-20 16:04:41 +0300

    Fix nginx_builder script

--- nginx_builder.sh
+++ nginx_builder.sh
@@ -6,13 +6,13 @@ DOCKER_REGISTRY=registry.dream:5001
 SECRET_USERNAME="$(docker secret ls | grep  "whale_username" | awk '{ print $2 }')"
 SECRET_PASSWORD="$(docker secret ls | grep  "whale_password" | awk '{ print $2 }')"
 
-if [ ! "$SECRET_USERNAME" != "whale_username" ]
+if [ "$SECRET_USERNAME" != "whale_username" ]
 then
     read -rp 'Please type username: ' WHALE_USERNAME
     echo "$WHALE_USERNAME" | docker secret create whale_username -
 fi
 
-if [ ! "$SECRET_PASSWORD" != "whale_password" ]
+if [ "$SECRET_PASSWORD" != "whale_password" ]
 then
     read -srp 'Please type your password: ' WHALE_PASSWORD
     echo "$WHALE_PASSWORD" | docker secret create whale_password -
